0b64704f5b7ddbdd5d2407bc05f011ac5aa2bba1
Grace Ejegwa <grayceecharles@gmail.com>
2022-03-31 16:43:55 +0100

    added cealrdb detials to database.js file

--- config/database.js
+++ config/database.js
@@ -52,22 +52,22 @@ module.exports = {
   */
   mysql: {
     client: 'mysql',
-    connection: {
-      host: Env.get('DB_HOST', ''),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', ''),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    },
+    // connection: {
+    //   host: Env.get('DB_HOST', ''),
+    //   port: Env.get('DB_PORT', ''),
+    //   user: Env.get('DB_USER', ''),
+    //   password: Env.get('DB_PASSWORD', ''),
+    //   database: Env.get('DB_DATABASE', 'adonis')
+    // },
 
     //connection for cleardb
-    // connection: {
-    //   host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
-    //   port: Env.get('DB_PORT', ''),
-    //   user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
-    //   password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
-    //   database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
-    // },
+    connection: {
+      host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
+      port: Env.get('DB_PORT', ''),
+      user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
+    },
     debug: Env.get('DB_DEBUG', false)
   },
 
